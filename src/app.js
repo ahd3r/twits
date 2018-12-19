2a67d5515c40e9cf80ea9c0639d82e53cda3a83e
Ander Stac <37026127+ahd3r@users.noreply.github.com>
2018-12-20 01:37:59 +0300

    Add files via upload

---
+++ src/app.js
@@ -0,0 +1,20 @@
+// const greeting = 'Hello World';
+// console.log(greeting);
+
+// const getData = async (url) => {
+//   const response = await fetch(url);
+//   const result = await response.json();
+//   return result;
+// };
+
+// getData('https://jsonplaceholder.typicode.com/posts').then(data=>console.log(data)).catch(err=>console.log(err));
+
+// const getStuffFromOtherFile = require('./module');
+// console.log(getStuffFromOtherFile);
+
+// import * as dataFromOtherFile from './module';
+
+// console.log(dataFromOtherFile.shit);
+// console.log(dataFromOtherFile.persone('Peyton', 'Female'));
+
+
---
+++ src/module.js
@@ -0,0 +1,7 @@
+// export const shit = 'shit';
+
+// export const persone=(name,sex)=>{
+// 	return {name:name,sex:sex};
+// };
+
+// module.exports='Root';
